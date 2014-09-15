462f9dc3188aabfb952063afbb818f4e0046f93a
Ron Shapiro <shapiro.rd@gmail.com>
2014-09-14 22:26:37 -0400

    Initial commit

---
+++ src/main/java/me/ronshapiro/rx/priority/PriorityScheduler.java
@@ -0,0 +1,155 @@
+package me.ronshapiro.rx.priority;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import rx.Scheduler;
+import rx.Scheduler.Worker;
+import rx.Subscription;
+import rx.functions.Action0;
+import rx.internal.schedulers.ScheduledAction;
+import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+/**
+ * A class to be used with RxJava's {@link Scheduler} interface. Though this class is not a {@link
+ * Scheduler} itself, calling {@link #priority(int)} will return one. E.x.: {@code PriorityScheduler
+ * scheduler = new PriorityScheduler(); Observable.just(1, 2, 3) .subscribeOn(scheduler.priority(10))
+ * .subscribe(); }
+ */
+public final class PriorityScheduler {
+
+    private final PriorityBlockingQueue<ComparableAction> queue;
+    private final AtomicInteger workerCount = new AtomicInteger();
+    private ExecutorService executorService;
+
+    public PriorityScheduler() {
+        this(null);
+    }
+
+    public PriorityScheduler(ExecutorService executorService) {
+        this.queue = new PriorityBlockingQueue<ComparableAction>();
+        this.executorService = executorService;
+    }
+
+    /**
+     * Prioritize {@link rx.functions.Action action}s with a numerical priority value. The higher
+     * the priority, the sooner it will run.
+     */
+    public Scheduler priority(final int priority) {
+        return new InnerPriorityScheduler(priority);
+    }
+
+    private final class InnerPriorityScheduler extends Scheduler {
+
+        private final int priority;
+
+        private InnerPriorityScheduler(int priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public Worker createWorker() {
+            synchronized (workerCount) {
+                if (workerCount.get() < parallelism()) {
+                    workerCount.incrementAndGet();
+                    if (executorService == null) {
+                        executorService = Executors.newFixedThreadPool(parallelism());
+                    }
+                    executorService.submit(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (true) {
+                                try {
+                                    ComparableAction action = queue.take();
+                                    action.call();
+                                } catch (InterruptedException e) {
+                                    Thread.currentThread().interrupt();
+                                    break;
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+            return new PriorityWorker(queue, priority);
+        }
+    }
+
+    private static final class PriorityWorker extends Worker {
+
+        private final CompositeSubscription compositeSubscription = new CompositeSubscription();
+        private final PriorityBlockingQueue<ComparableAction> queue;
+        private final int priority;
+
+        private PriorityWorker(PriorityBlockingQueue<ComparableAction> queue, int priority) {
+            this.queue = queue;
+            this.priority = priority;
+        }
+
+        @Override
+        public Subscription schedule(Action0 action) {
+            return schedule(action, 0, MILLISECONDS);
+        }
+
+        /**
+         * inspired by HandlerThreadScheduler.InnerHandlerThreadScheduler#schedule.
+         *
+         * @see <a href="https://github.com/ReactiveX/RxAndroid/blob/53bc70785b1c8f150c2be871a5b85979ad8b233a/src/main/java/rx/android/schedulers/HandlerThreadScheduler.java">InnerHandlerThreadScheduler</a>
+         */
+        @Override
+        public Subscription schedule(Action0 action, long delayTime, TimeUnit unit) {
+            final ComparableAction comparableAction = new ComparableAction(action, priority);
+
+            final ScheduledAction scheduledAction = new ScheduledAction(comparableAction);
+            scheduledAction.add(Subscriptions.create(new Action0() {
+                @Override
+                public void call() {
+                    queue.remove(comparableAction);
+                }
+            }));
+            scheduledAction.addParent(compositeSubscription);
+            compositeSubscription.add(scheduledAction);
+
+            queue.offer(comparableAction, delayTime, unit);
+            return scheduledAction;
+        }
+
+        @Override
+        public void unsubscribe() {
+            compositeSubscription.unsubscribe();
+        }
+
+        @Override
+        public boolean isUnsubscribed() {
+            return compositeSubscription.isUnsubscribed();
+        }
+
+    }
+
+    private static final class ComparableAction implements Action0, Comparable<ComparableAction> {
+
+        private final Action0 action;
+        private final int priority;
+
+        private ComparableAction(Action0 action, int priority) {
+            this.action = action;
+            this.priority = priority;
+        }
+
+        @Override
+        public void call() {
+            action.call();
+        }
+
+        @Override
+        public int compareTo(ComparableAction o) {
+            return o.priority - priority;
+        }
+    }
+}
---
+++ src/test/java/me/ronshapiro/rx/priority/PrioritySchedulerTest.java
@@ -0,0 +1,69 @@
+package me.ronshapiro.rx.priority;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+import static org.junit.Assert.fail;
+
+public class PrioritySchedulerTest {
+
+    @Test
+    public void schedulesInOrderOfPriority() throws Exception {
+        PriorityScheduler scheduler = new PriorityScheduler(Executors.newSingleThreadExecutor());
+
+        final int count = 1000;
+        final CountDownLatch finishLatch = new CountDownLatch(count);
+        final CountDownLatch loopLatch = new CountDownLatch(1);
+        final List<Integer> actual = Collections.synchronizedList(new ArrayList<Integer>());
+        final Object onNextLock = new Object();
+
+        for (int i = 0; i < count; i++) {
+            Observable.just(i)
+                    .subscribeOn(scheduler.priority(i))
+                    .observeOn(Schedulers.immediate())
+                    .subscribe(new Action1<Integer>() {
+                        @Override
+                        public void call(Integer integer) {
+                            synchronized (onNextLock) {
+                                await(loopLatch);
+                                actual.add(integer);
+                                finishLatch.countDown();
+                            }
+                        }
+                    });
+        }
+        loopLatch.countDown();
+        finishLatch.await();
+
+        final int parallelism = scheduler.priority(0).parallelism();
+        List<Integer> subList = actual.subList(parallelism, actual.size());
+        int last = Integer.MAX_VALUE;
+        for (int i : subList) {
+            if (last < i) {
+                System.out.println("actual: " + actual);
+                fail("actual was not monotonically decreasing after the first N items, where N " +
+                        "is the scheduler's parallelism/number of the cores on the machine. " +
+                        "failed at index " + actual.indexOf(i) + " (value = " + i + "). " +
+                        "Full list: " + actual);
+            }
+            last = i;
+        }
+    }
+
+    private static void await(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
